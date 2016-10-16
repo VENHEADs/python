2c336e3d772eb3c66a70f20fb8ce07e15ceee9dc
VENHEADs <VENHEADs@yandex.ru>
2016-10-16 17:47:46 +0300

    Update approximation.py

--- approximation.py
+++ approximation.py
@@ -2,25 +2,31 @@ import numpy as np
 import scipy
 from scipy import sin,cos,exp
 import scipy.linalg
-In [183]:
+from scipy import interpolate
+from matplotlib import pylab as plt
 fx = lambda x: sin(x / 5.0) * exp(x / 10.0) + 5 * exp(-x / 2.0)
-In [184]:
-p1 = np.array([1,15])
-b1 = fx(p1)
-In [185]:
-a1 = np.array([[p1[0]**n for n in xrange(0,2)], [p1[1]**n for n in xrange(0,2)]])
-In [186]:
-s1 = scipy.linalg.solve(a1,b1)
-In [ ]:
- 
-In [187]:
-b =  np.row_stack((p1[0], p1[1]))
-In [ ]:
- 
-In [188]:
-p3 = np.array([1,4,10,15])
-In [189]:
-b3 = fx(p3)
-In [190]:
-a3 = np.array([[p3[0]**n for n in xrange(0,4)], [p3[1]**n for n in xrange(0,4)], [p3[2]**n for n in xrange(0,4)], 
-               [p3[3]**n for n in xrange(0,4)]])
+p = np.array([1,15])
+b = fx(p)
+k=2
+a = np.zeros((len(p), len(p)))
+for i in range(0,k):
+    a[i,:] = np.array([p[i]**n for n in xrange(0,k)])
+s1 = scipy.linalg.solve(a,b)
+p = np.array([1,4,8,15])
+b = fx(p)
+k=4
+a = np.zeros((len(p), len(p)))
+for i in range(0,k):
+    a[i,:] = np.array([p[i]**n for n in xrange(0,k)])
+s3 = scipy.linalg.solve(a,b)
+f = interpolate.interp1d(p, fx(p), kind='quadratic')
+xnew = np.arange(0, 15, 0.1)
+plt.plot(p, b, 'x', p, b, '----')
+plt.show()
+p = np.array([1,8,15])
+b = fx(p)
+k=3
+a = np.zeros((len(p), len(p)))
+for i in range(0,k):
+    a[i,:] = np.array([p[i]**n for n in xrange(0,k)])
+s2 = scipy.linalg.solve(a,b)
