f330eb12acd41facf3286f607f6ff0e84e6736b4
VENHEADs <VENHEADs@yandex.ru>
2016-10-16 17:50:24 +0300

    Update approximation.py

--- approximation.py
+++ approximation.py
@@ -4,13 +4,13 @@ from scipy import sin,cos,exp
 import scipy.linalg
 from scipy import interpolate
 from matplotlib import pylab as plt
-fx = lambda x: sin(x / 5.0) * exp(x / 10.0) + 5 * exp(-x / 2.0)
+fx = lambda x: sin(x / 5.0) * exp(x / 10.0) + 5 * exp(-x / 2.0) #задачем функцию
 p = np.array([1,15])
 b = fx(p)
 k=2
-a = np.zeros((len(p), len(p)))
-for i in range(0,k):
-    a[i,:] = np.array([p[i]**n for n in xrange(0,k)])
+a = np.zeros((len(p), len(p)))  #создаем пустой массив нужной размерности
+for i in range(0,k):            # заполняем массив
+    a[i,:] = np.array([p[i]**n for n in xrange(0,k)]) # заполняем массив
 s1 = scipy.linalg.solve(a,b)
 p = np.array([1,4,8,15])
 b = fx(p)
