bfda42940b699f5769b0e1679dfd55a03ff3f595
VENHEADs <VENHEADs@yandex.ru>
2016-10-21 22:58:14 +0300

    Update Programming Assignment: Оптимизация в Python: глобальная оптимизация и оптимизация негладкой функции.py

--- Programming Assignment: Оптимизация в Python: глобальная оптимизация и оптимизация негладкой функции.py
+++ Programming Assignment: Оптимизация в Python: глобальная оптимизация и оптимизация негладкой функции.py
@@ -6,7 +6,9 @@ from scipy import interpolate
 from matplotlib import pylab as plt
 from scipy import optimize
 fx = lambda x: sin(x / 5.0) * exp(x / 10.0) + 5.0 * exp(-x / 2.0) #задаем функцию
-apx = scipy.optimize.minimize (fx,30,method="BFGS") # минимищируем с приближением 30 можно сравнить с рпиближением 2
+apx = scipy.optimize.minimize (fx,30,method="BFGS") # минимищируем с приближением 30 можно сравнить с приближеением 2
 x = np.arange(0, 30, 0.1)  #стриом функцию
 plt.plot(x, fx(x)) #выводим график
 plt.show() #выводим
+bounds = [(1,30)]
+scipy.optimize.differential_evolution(fx,bounds) # алгоритм дифференциальной эволюции
