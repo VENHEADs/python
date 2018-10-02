52e295d856553ec08a58f26c64bd073bc51e220e
Valeriy Babushkin <VENHEADs@yandex.ru>
2018-10-02 23:22:33 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,30 @@
+# Настройки
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import apiai, json
+updater = Updater(token='484814346:AAFXtoYPhZTBqSRzBZiKRbkO5JeyKBsnqlg') # Токен API к Telegram
+dispatcher = updater.dispatcher
+# Обработка команд
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+def textMessage(bot, update):
+    request = apiai.ApiAI('dca6e3be475a4d7c94e70a7052f3c0a8').text_request() # Токен API к Dialogflow
+    request.lang = 'ru' # На каком языке будет послан запрос
+    request.session_id = 'BatlabAIBot' # ID Сессии диалога (нужно, чтобы потом учить бота)
+    request.query = update.message.text # Посылаем запрос к ИИ с сообщением от юзера
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
+    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+    if response:
+        bot.send_message(chat_id=update.message.chat_id, text=response)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
+# Хендлеры
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+# Добавляем хендлеры в диспетчер
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+# Начинаем поиск обновлений
+updater.start_polling(clean=True)
+# Останавливаем бота, если были нажаты Ctrl + C
+updater.idle()
